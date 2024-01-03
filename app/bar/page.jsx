f52ce59814b1a5479f2ff488fc2ec0008bfac83e
Rdebora <riosdeborasabrina@gmail.com>
2024-01-03 11:38:03 -0300

    actualizacion portafolio

--- app/bar/page.jsx
+++ app/bar/page.jsx
@@ -40,7 +40,7 @@ export default function Sidebar() {
           </button>
         </a>{" "}
         <a
-          href="Rios Debora Sabrina (3).pdf"
+          href="Rios Debora Sabrina (4).pdf"
           download
           target="_blank"
           rel="noopener noreferrer"
