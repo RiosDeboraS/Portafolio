1bf5a0d56f51cb66ba00c5f12a0bec75cf7b852d
Rdebora <riosdeborasabrina@gmail.com>
2024-01-04 19:58:54 -0300

    afgregamos part

--- app/page.jsx
+++ app/page.jsx
@@ -5,10 +5,12 @@ import style from "./home.module.css";
 import Mostrar from "./mostrar/page";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import Footer from "./footer/page";
+import ParticlesWall from "@/component/wallpeaper.jsx/ParticlesWall";
 
 export default function Portafolio() {
   return (
     <div className={style.container}>
+      <ParticlesWall></ParticlesWall>
       <Sidebar></Sidebar>
       <About></About>
       <Proyecto></Proyecto>
